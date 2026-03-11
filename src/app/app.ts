85375164ba29f1153f7142137990ff6d3eb7e4c6
mertokumus01 <okumus01mert@hotmail.com>
2026-03-11 13:30:39 +0300

    COMPONENT EKLEME

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
+import { MoviesComponent } from './movies/movies';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ MoviesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MovieApp');
+
 }
---
+++ src/app/movies/movies.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-movies",//bu kodu şöyle çağırırız :
+    templateUrl:"./movies.html"
+})
+export class MoviesComponent {
+
+}
