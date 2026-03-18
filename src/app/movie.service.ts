0738a923f620906918aa83677dbd547a3f83685b
mertokumus01 <okumus01mert@hotmail.com>
2026-03-18 14:39:52 +0300

    http eklentisi

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -4,11 +4,16 @@ import { Movies } from './movie.datasource';
 import { Observable, of } from 'rxjs';
 import { Logging } from './logging/logging';
 import { LoggingService } from './logging.service';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
-  constructor(public loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private http: HttpClient
+  ) {}
   getMovies(): Observable<MovieType[]> {
     this.loggingService.addMessage('MovieService: listing movies');
     return of(Movies);
@@ -19,4 +24,14 @@ export class MovieService {
   return of(Movies.find(movie => movie.id === id)!);
 }
 
+update(movie:MovieType): Observable<any>{
+  this.loggingService.addMessage('MovieService: update movie id='+movie.id)
+  const index = Movies.findIndex(m => m.id === movie.id);
+  if (index !== -1) {
+    Movies[index] = movie;
+    return of({ success: true });
+  } else {
+    return of({ success: false, message: 'Movie not found' });
+  }
+}
 }
